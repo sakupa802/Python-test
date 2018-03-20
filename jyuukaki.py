85fbcc8f0ca739b53d3534fdf269f74af652c5e8
hiroki.akazaki_gpol <hiroki.akazaki@gpol.co.jp>
2018-03-20 19:25:58 +0900

    [modify] 重回帰実装

---
+++ jyuukaki.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import StandardScaler
+
+#ボストンデータ読み込み
+boston = load_boston()
+df = pd.DataFrame(boston.data, columns = boston.feature_names)
+df['target'] = boston.target
+
+#学習データ準備
+f_num = df.shape[1] - 1 #(目的変数の数)
+train_X = np.array(df.iloc[:, :f_num])
+train_Y = np.array(df.iloc[:, f_num: f_num + 1])
+n_samples = train_X.shape[0]
+
+#正規化
+ss = StandardScaler()
+ss.fit(train_X)
+train_X = ss.transform(train_X)
+
+#プレースホルダー
+with tf.name_scope('data'):
+    X = tf.placeholder(tf.float32, shape = [None, f_num ], name = "X")
+    Y = tf.placeholder(tf.float32, name = "Y")
+
+#変数（パラメータ）
+with tf.name_scope('parameter'):
+    W = tf.Variable(tf.zeros([f_num, 1]), name = "weight")
+    b = tf.Variable(tf.zeros([1]), name = "bias")
+
+#モデル
+with tf.name_scope('model'):
+    pred = tf.add(tf.matmul(X, W), b) #matmulは行列の積
+
+#損失関数
+with tf.name_scope('loss'):
+    # Mean squared error
+    loss = tf.reduce_mean(tf.square(pred - Y))
+    tf.summary.scalar('loss', loss)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+train_step = optimizer.minimize(loss)
+
+#決定係数(R2)
+with tf.name_scope('r2'):
+    r2 = 1 - (tf.reduce_sum(tf.square(Y - pred)) / tf.reduce_sum(tf.square(Y - tf.reduce_mean(Y))))
+    tf.summary.scalar('r2', r2)
+
+with tf.Session() as sess:
+    # ログの設定
+    summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("boston_log", sess.graph)
+    
+    sess.run(tf.global_variables_initializer())#変数初期化
+    
+    for i in range(1000):
+        sess.run(train_step, feed_dict={X: train_X, Y: train_Y})
+        if i != 0 and i % 200 == 0: # 200ステップごとに精度を出力
+            train_summary, train_loss, train_r2 = sess.run([summary, loss, r2], feed_dict={X: train_X, Y:train_Y})# コストと精度を出力
+            writer.add_summary(train_summary, i) #summaryの更新
+            
+            print("Step:", '%04d' % (i), "loss=", "{:.9f}".format(train_loss), "r2=", "{:.9f}".format(train_r2), "W=", sess.run(W), "b=", sess.run(b))            
+            
+    training_cost, training_r2 = sess.run([loss,r2], feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "Training r2=", training_r2, "W=", sess.run(W), "b=", sess.run(b), '\n')
--- kikagaku3.py
+++ kikagaku3.py
@@ -2,67 +2,49 @@ import tensorflow as tf
 import numpy as np
 # matplotlib パッケージを読み込み
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-# データを生成
-x_data = np.asarray([[10,20,30], [24,80,10], [30,40,9], [40,25,15], [51,80,70], [60,80,50]])
+# # データを生成
+x_data = np.asarray([[10,20,30], [24,80,10], [30,40,9], [40,25,15], [51,80,70], [60,80,50]], dtype=np.float32)
 #x_data = x_data * 0.01 # 結果がNaN(無限大)になるので、0.01掛ける
-# 物件の家賃
-y_data = np.asarray([[103], [242], [304], [402], [519], [625]])
-#y_data = y_data * 0.01 # 結果がNaN(無限大)になるので、0.01掛ける
+# # 物件の家賃
+y_data = np.asarray([[103], [242], [304], [402], [519], [625]], dtype=np.float32)
+#y_data = y_data * 0.01 # 結果がNaN(無限大)になるので、0.01掛ける#正規化
 
 n_1 = len(x_data)#6
-n_2 = len(x_data[0])#3
+n_2 = len(x_data[0])#3 (目的変数の数)
+
+X = tf.placeholder(tf.float32, shape = [None, n_2], name = "X")
+Y = tf.placeholder(tf.float32, name = "Y")
 
 # モデル
-#x_d = tf.placeholder(tf.float32, [None,n])
-#y_d = tf.placeholder(tf.float32, [None,1])
-w = tf.Variable(tf.zeros([n_1, n_2]))
+W = tf.Variable(tf.zeros([n_2, 1]))
 b = tf.Variable(tf.zeros([1]), name = "bias")
-y_hat = w * x_data + b
+y_hat = tf.add(tf.matmul(X, W), b)
+#tf.transpose()
 
 # 目的関数
-loss = tf.reduce_sum(tf.square(y_data - (y_hat)))
+loss = tf.reduce_sum(tf.square(y_data - y_hat))
 
 # 確率的最急勾配法
 rate = 0.5
 optimizer = tf.train.AdamOptimizer()
 train = optimizer.minimize(loss)
 
-# 変数初期化
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())#変数初期化
 
-for step in range(42001):
-    loss_val = sess.run(train)
-    if step % 2000 == 0:
-        loss_val = sess.run(loss)
-        print('Step: %03d,   Loss: %5.4f' % (step,loss_val), sess.run(w))
-        sess.run(w)
-        sess.run(b)
-
-# # 目的関数
-# loss = tf.reduce_sum(tf.square(y_data - y_hat))
-
-# # 確率的最急勾配法
-# rate = 0.5
-# optimizer = tf.train.AdamOptimizer()
-# train = optimizer.minimize(loss)
-
-
-# # 変数初期化
-# init = tf.global_variables_initializer()
-# sess = tf.Session()
-# sess.run(init)
-
-# for step in range(12001):
-#     if step % 1000 == 0:
-#         loss_val = sess.run(loss, feed_dict={y_hat:y_train}) 
-#         print('Step: %03d,   Loss: %5.4f' % (step,loss_val))
-#         w_val = sess.run(w)
-#     sess.run(train, feed_dict={x_d:x_train,y_d:y_train})
-
-# sess.close()
+    for step in range(120001):
+        sess.run(train, feed_dict={X: x_data, Y:y_data})
+        if step % 10000 == 0:
+            loss_val = sess.run(loss, feed_dict={X: x_data, Y:y_data}) 
+            print('Step: %03d,   Loss: %5.4f' % (step,loss_val))
+            w_val = sess.run(W)
+            b_val = sess.run(b)
+            print(w_val)
+            print(b_val)
 
 # # 線形回帰予測関数
 # def predict(x):
